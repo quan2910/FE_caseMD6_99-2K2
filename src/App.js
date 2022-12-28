8d342fcb4a623879c7225101607018189f66b309
quan2910 <ngtruongquan@gmail.com>
2022-12-28 10:15:37 +0700

    register done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
   return (
     <div>
-      <Login></Login>
+      <Register></Register>
     </div>
   );
 }
--- src/pages/login.js
+++ src/pages/login.js
@@ -3,6 +3,7 @@ import {Field, Form, Formik} from "formik";
 import {Link, useNavigate} from "react-router-dom";
 import {useDispatch, useSelector} from "react-redux";
 import {login} from "../service/userService";
+import "../../src/style/loginCSS.css"
 
 
 function Login(props) {
@@ -11,7 +12,30 @@ function Login(props) {
 
     return (
         <div>
+            <div className="container" id="container">
+                <div className="form-container sign-in-container">
+                    <Formik>
+                        <Form action="#">
+                            <h1>LogIn</h1>
+                            <br/>
+                            <Field type="text" name={"username"} placeholder="Username"/>
+                            <Field type="password" name={"password"} placeholder="Password"/>
+                            <button>Login</button>
+                        </Form>
+                    </Formik>
+                </div>
+                <div className="overlay-container">
+                    <div className="overlay">
+                        <div className="overlay-panel overlay-right">
+                            <h1>Hello, Friend!</h1>
+                            <p>Enter your personal details and start journey with us</p>
+                            <button className="ghost" id="signUp">Register</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
+
 export default Login;
