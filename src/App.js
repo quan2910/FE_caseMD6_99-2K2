df7cb493dbb1f77d1a86b19e326be37995afe5eb
unknown <you@example.com>
2022-12-29 09:06:20 +0700

    git sáng sớm

--- src/App.js
+++ src/App.js
@@ -4,8 +4,23 @@ import Home from "./pages/home/Home";
 import CreateWallet from "./pages/wallet/CreateWallet";
 import Login from "./pages/user/login";
 import Register from "./pages/user/register";
+import {useEffect} from "react";
+import axios from "axios";
+import {useSelector} from "react-redux";
 
 function App() {
+
+    const idUser = useSelector(state => {
+        return state.user.currentUser.user.authenticUser[0].idUser
+    })
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/wallet/find-by-idUser/' + idUser).then(response => {
+            const wallets = response.data
+            console.log('vào App')
+            console.log('wallets', wallets);
+        })
+    }, [1])
     return (
         <div className={"col-12"}>
             {/* ======= Header ======= */}
