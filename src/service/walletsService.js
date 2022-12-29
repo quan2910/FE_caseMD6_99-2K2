a08871b671c6616914e04cf9029593bc6e9c9cb9
Nguyen9c1999 <nguyen9c1999@gmail.com>
2022-12-29 11:36:58 +0700

    em quan sua lai api

--- src/service/walletsService.js
+++ src/service/walletsService.js
@@ -1,10 +1,11 @@
 import {createAsyncThunk} from "@reduxjs/toolkit";
-import customAxios from "./api";
+
+import axios from "axios";
 
 export const getWallets = createAsyncThunk(
     'wallets/getWallets',
     async () => {
-        const res = await customAxios.get('wallet')
+        const res = await axios.get('http://localhost:3000/wallet')
         console.log(res)
         return res.data
     }
@@ -13,7 +14,7 @@ export const getWallets = createAsyncThunk(
 export const addWallets = createAsyncThunk(
     'wallets/addWallets',
     async (data) => {
-        const res = await customAxios.post('wallet/create', data)
+        const res = await axios.post('http://localhost:3000/wallet/create', data)
         console.log('data', data)
         console.log('res', res)
         return res
