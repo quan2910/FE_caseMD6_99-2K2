c120cd40fd1becc25eece5911289bebd214fad81
quan2910 <ngtruongquan@gmail.com>
2023-01-03 09:57:17 +0700

    donee sp1

--- src/pages/user/change-password.js
+++ src/pages/user/change-password.js
@@ -1,13 +1,12 @@
 import {Link, useNavigate} from "react-router-dom";
 import {Field, Form, Formik} from "formik";
 import {toast, ToastContainer} from "react-toastify";
-import React, {useState} from "react";
+import React from "react";
 import "../../style/loginCSS.css"
 import * as Yup from "yup";
 import 'react-toastify/dist/ReactToastify.css';
 import {useDispatch, useSelector} from "react-redux";
 import {changePassword} from "../../service/userService";
-
 
 const SignupSchema = Yup.object().shape({
     oldPassword: Yup.string()
@@ -25,9 +24,12 @@ const SignupSchema = Yup.object().shape({
 });
 
 export default function ChangePassword() {
+    const idUser = useSelector(state => {
+        console.log(state.user.currentUser.user.authenticUser[0].idUser)
+        return state.user.currentUser.user.authenticUser[0].idUser
+    })
     const dispatch = useDispatch()
     const navigate = useNavigate()
-
     const showToastMessage = async () => {
         await toast.success(' Changed Password!', {
             position: "top-center",
@@ -40,53 +42,43 @@ export default function ChangePassword() {
             theme: "light",
         });
     };
-    const showToastMessage1 = async () => {
-        await toast.success(' Current Password is wrong!', {
-            position: "top-center",
-            autoClose: 1500,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-            theme: "light",
-        });
-    };
-    let mkC = useSelector(state => {
-        console.log(state.user.currentUser.user.authenticUser[0].password)
-    })
-    const handleChangePassword = async (values,id) => {
 
-    }
     return (
         <div style={{marginTop: 70}}>
             <div className="container" id="container">
                 <div>
                     <Formik validationSchema={SignupSchema} initialValues={{
                         oldPassword: "",
-                        newPassword: "",
-                        rePassword: ""
-                    }} onSubmit={async (values, {resetForm}) => {
-                        await handleChangePassword(values, resetForm)
+                        newPassword: ""
+                    }}onSubmit={async (values,{resetForm}) => {
+                        let data = await dispatch(changePassword({...values, idUser}))
+                        console.log("data",data)
+                        if(data.payload.user.check) {
+                            await showToastMessage();
+                            navigate("/home")
+                        } else {
+                            alert(data.payload.mess)
+                            resetForm()
+                        }
                     }}>
-                        {({errors, touched}) => (
+                        {({errors, touched}) =>(
                             <Form>
                                 <h1 style={{color: "black", marginTop: 100}}>Change Password</h1>
                                 <br/>
                                 <Field type="password" name={"oldPassword"} placeholder="OldPassword"
                                        style={{backgroundColor: "lightgrey"}}/>
                                 {errors.oldPassword && touched.oldPassword ? (
-                                    <span style={{color: "red"}}>{errors.oldPassword}</span>
+                                    <span style={{color:"red"}}>{errors.oldPassword}</span>
                                 ) : null}
                                 <Field type="password" name={"newPassword"} placeholder="NewPassword"
                                        style={{backgroundColor: "lightgrey"}}/>
                                 {errors.newPassword && touched.newPassword ? (
-                                    <span style={{color: "red"}}>{errors.newPassword}</span>
+                                    <span style={{color:"red"}}>{errors.newPassword}</span>
                                 ) : null}
                                 <Field type="password" name={"rePassword"} placeholder="Re-NewPassword"
                                        style={{backgroundColor: "lightgrey"}}/>
                                 {errors.rePassword && touched.rePassword ? (
-                                    <span style={{color: "red"}}>{errors.rePassword}</span>
+                                    <span style={{color:"red"}}>{errors.rePassword}</span>
                                 ) : null}
                                 <br/>
                                 <button>Save</button>
