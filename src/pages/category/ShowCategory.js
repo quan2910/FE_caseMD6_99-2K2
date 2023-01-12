259d8684120131baa52b98f9351d60b018374d43
quan2910 <ngtruongquan@gmail.com>
2023-01-12 14:34:00 +0700

    bao tri lan 2

--- src/pages/category/ShowCategory.js
+++ src/pages/category/ShowCategory.js
@@ -56,7 +56,7 @@ export default function ShowCategory() {
         dispatch(showDetailWallet(user.idUser))
     }, [])
     return (
-        <div className="row" style={{marginLeft: 180}}>
+        <div className="row" style={{marginLeft: 200}}>
             <button style={{width: 200, marginBottom: 20, marginLeft: 12, background: "#82AAE3"}}>
                 <CreateCategory></CreateCategory>
             </button>
